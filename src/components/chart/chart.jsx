0c60b6c4a9b825abf510f1e76769d118319577a3
Irfan Khan <officialirfanafzal@gmail.com>
2020-04-18 18:04:04 +0530

    added react-charts initial file

--- src/components/chart/chart.jsx
+++ src/components/chart/chart.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api/index";
+import { Bar, Line } from "react-chartjs-2";
+import styles from "./chart.module.css";
 
 const Chart = () => {
-    return <h1>Chart</h1>;
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        };
+        fetchAPI();
+    });
 };
+
+const lineChart = dailyData[0] ? (
+    <line data={{ labels: "", datasets: [{}, {}] }} />
+) : null;
 
 export default Chart;
