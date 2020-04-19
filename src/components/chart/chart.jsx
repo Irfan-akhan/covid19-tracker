7e070c3a341dae85a43b6607615ee677bc109a76
Irfan Khan <officialirfanafzal@gmail.com>
2020-04-19 16:40:36 +0530

    finished  country picker component

--- src/components/chart/chart.jsx
+++ src/components/chart/chart.jsx
@@ -3,16 +3,16 @@ import { fetchDailyData } from "../../api/index";
 import { Bar, Line } from "react-chartjs-2";
 import styles from "./chart.module.css";
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     const [dailyData, setDailyData] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
         };
-        console.log(dailyData[0]);
+        console.log(dailyData);
         fetchAPI();
-    }, [dailyData]);
+    }, []);
 
     const lineChart = dailyData.length ? (
         <Line
@@ -38,6 +38,32 @@ const Chart = () => {
         />
     ) : null;
 
-    return <div className={styles.container}>{lineChart}</div>;
+    const barChart = confirmed ? (
+        <Bar
+            data={{
+                labels: ["Infected", "Recovered", "Deaths"],
+                datasets: [
+                    {
+                        label: "people",
+                        backgroundColor: [
+                            "rgba(0, 0, 255, 0.5)",
+                            "rgba(255, 0, 0, 0.5)",
+                            "rgba(0, 255, 0, 0.5)",
+                        ],
+                        data: [confirmed.value, recovered.value, deaths.value],
+                    },
+                ],
+            }}
+            options={{
+                legend: { display: false },
+                title: {
+                    display: true,
+                    title: { display: true, text: `Current state in ${country}` },
+                },
+            }}
+        />
+    ) : null;
+
+    return <div className={styles.container}>{country ? barChart : lineChart}</div>;
 };
 export default Chart;
--- src/components/countryPicker/CountryPicker.jsx
+++ src/components/countryPicker/CountryPicker.jsx
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { NativeSelect, FormControl } from "@material-ui/core";
-import { styles } from "./CountryPicker.module.css";
+import styles from "./CountryPicker.module.css";
+import { fetchCountries } from "../../api/index";
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
+    const [fetchedCountries, setFetchCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFetchCountries(await fetchCountries());
+        };
+        fetchAPI();
+    }, [setFetchCountries]);
+    console.log(fetchedCountries);
+
     return (
         <FormControl className={styles.formControl}>
-            <NativeSelect>
-                <option value="global">Global</option>
+            <NativeSelect
+                defaultValue=""
+                onChange={e => {
+                    console.log(e.target.value);
+                    handleCountryChange(e.target.value);
+                }}
+            >
+                <option key={1} value="global">
+                    Global
+                </option>
+                {fetchedCountries.map((country, i) => (
+                    <option key={i} value={country}>
+                        {country}
+                    </option>
+                ))}
             </NativeSelect>
         </FormControl>
     );
